7cbc8b8863569018b624145c76e63ab16715984a
allen-s49 <546886446@qq.com>
2016-12-18 16:49:19 +0800

    add base.html

--- blog/models.py
+++ blog/models.py
@@ -3,6 +3,8 @@ from django.db import models
 # Create your models here.
 class Blog(models.Model):
 	blog_title = models.CharField(max_length=50)
+	blog_tag = models.CharField(max_length=20)
 	blog_content = models.TextField()
 	pub_date = models.DateTimeField('date published')
 	update_date = models.DateTimeField('date updated')
+	read_counts = models.IntegerField()
